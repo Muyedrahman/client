e1f2dcc7222e23d1eff9b33b53bc347c16a01842
Muyedur rahman <muyedrahman732@gmail.com>
2026-04-26 21:22:17 +0600

    add Donor Home

---
+++ src/components/Dashboard/Statistics/DonorHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DonorHome = () => {
+    return (
+      <div className='p-5'>
+        <h2>Welcome back, Rahim! 👋</h2>
+        {/* ৩টা recent donation request table */}
+      </div>
+    );
+};
+
+export default DonorHome;
