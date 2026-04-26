6b69adc9383e7b96370adb6975019fea84d47705
Muyedur rahman <muyedrahman732@gmail.com>
2026-04-26 21:24:26 +0600

    add Admin Home

---
+++ src/components/Dashboard/Statistics/AdminHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminHome = () => {
+    return (
+      <div className="p-5">
+        <h2>Welcome, Admin! 👑</h2>
+        {/* ৩টা Stats Card */}
+      </div>
+    );
+};
+
+export default AdminHome;
